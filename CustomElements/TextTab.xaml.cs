e0b04eba91bf53cba12cd032e792919fe323f183
dancing-bug <dancing-bug@outlook.com>
2023-11-10 23:47:29 +0100

    Initial commit

---
+++ CustomElements/TextTab.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Point = System.Windows.Point;
+
+namespace CorpusSearchEngine.CustomElements
+{
+    /// <summary>
+    /// Interaction logic for TextTab.xaml
+    /// </summary>
+    public partial class TextTab : UserControl
+    {
+        string[] fullText = new string[3]; 
+        string[] shortText = new string[3];
+        bool isShortened = true;
+        int tabState = 0;
+        SolidColorBrush redBrush = new SolidColorBrush(System.Windows.Media.Color.FromArgb(255, 240, 79, 95));
+        SolidColorBrush greenBrush = new SolidColorBrush(System.Windows.Media.Color.FromArgb(255, 113, 217, 121));
+
+        //System.Windows.Media.Color highlight = System.Windows.Media.Color.FromRgb(114, 207, 103);
+        public TextTab()
+        {
+            InitializeComponent();
+        }
+
+        public string bind
+        {
+            get { return Bind.Text; }
+            set { Bind.Text = value; }
+        }
+        
+        public string number
+        {
+            get { return Number.Text; }
+            set { Number.Text = value; }
+        }
+
+
+        public string content
+        {
+            get { return Content2.Text; }
+            set { Content2.Text = value;}
+        }
+
+        public string[] highlightedContent
+        {
+            set
+            {
+                ProcessTextParts(value);
+            }
+        }
+
+        public void ProcessTextParts(string[] content)
+        {
+            fullText = content;
+            if(Math.Max(0, content[0].Length - 300) > 0)
+            {
+                shortText[0] = "(...) " + content[0].Substring(content[0].Length - 300);
+            }
+            else
+            {
+                shortText[0] = content[0].Substring(0);
+            }
+            //shortText[0] = "... " + content[0].Substring(Math.Max(0, content[0].Length - 300));
+            shortText[1] = content[1];
+
+            if (Math.Min(content[2].Length, 300) < 300)
+            {
+                shortText[2] = content[2].Substring(0, content[2].Length);
+            }
+            else
+            {
+                shortText[2] = content[2].Substring(0, 300) + " (...)";
+            }
+            //shortText[2] = content[2].Substring(0, Math.Min(content[2].Length,  300)) + "...";
+
+            isShortened = true;
+            HighlightWord(shortText);
+        }
+
+        public void HighlightWord(string[] content)
+        {
+            Content1.Text = content[0];
+            Content2.Text = content[1];
+            Content2.Background = Brushes.PaleGreen;
+            Content3.Text = content[2];
+        }
+
+        private void FlipExpandImage(int scale)
+        {
+            expandImg.RenderTransformOrigin = new Point(0.5, 0.5);
+            ScaleTransform flipTransform = new ScaleTransform();
+            flipTransform.ScaleY = scale;
+            expandImg.RenderTransform = flipTransform;
+        }
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(Bind.Text + " " + Number.Text + " " + Content1.Text + Content2.Text + Content3.Text);
+        }
+
+        private void ExpandButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isShortened)
+            {
+                Content1.Text = fullText[0];
+                Content2.Text = fullText[1];
+                Content3.Text = fullText[2];
+                isShortened = false;
+
+                FlipExpandImage(-1);
+            }
+            else
+            {
+                Content1.Text = shortText[0];
+                Content2.Text = shortText[1];
+                Content3.Text = shortText[2];
+                isShortened = true;
+
+                FlipExpandImage(1);
+            }
+        }
+
+        private void TabButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (tabState == 0)
+            {
+                tabState++;
+                tabBorder.Background = redBrush;
+            }
+            else if (tabState == 1)
+            {
+                tabState++;
+                tabBorder.Background = greenBrush;
+            }
+            else
+            {
+                tabState = 0;
+                tabBorder.Background = Brushes.Transparent;
+            }
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,239 @@
+﻿using CorpusSearchEngine.CustomElements;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CorpusSearchEngine
+{
+    public partial class MainWindow : Window
+    {
+        int cnt = 1;
+        string word = string.Empty;
+        bool isSearching = false;
+        int wordsCount = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        async void searchButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isSearching)
+            {
+                isSearching = true;
+
+                word = searchTextbox.Text;
+                canvasStackPanel.Children.Clear();
+
+                await Task.Run(() => SearchForWord(word));
+            }
+        }
+
+        async void searchTextbox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (!isSearching)
+                {
+                    isSearching = true;
+
+                    word = searchTextbox.Text;
+                    canvasStackPanel.Children.Clear();
+
+                    await Task.Run(() => SearchForWord(word));
+                }
+            }
+
+        }
+
+        private string ReadFile(string path)
+        {
+            using (var reader = File.OpenText(path))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        private void UpdateResultText(string text)
+        {
+            resultText.Text = text;
+        }
+
+        private void AddNewEntry(string bind, string number, string content)
+        {
+            canvasStackPanel.Children.Add(new TextTab { bind = bind, number = number, content = content});
+        }
+
+        private void AddNewEntry(string bind, string number, string[] content)
+        {
+            canvasStackPanel.Children.Add(new TextTab { bind = bind, number = number, highlightedContent = content});
+        }
+
+        private string[] DivideText(string text, string word)
+        {
+            int index = text.IndexOf(word, StringComparison.OrdinalIgnoreCase);
+
+            if (index == -1)
+            {
+                // Word not found in the text
+                return new string[] { text, string.Empty, string.Empty };
+            }
+
+            string[] dividedParts = new string[3];
+
+            dividedParts[0] = text.Substring(0, index);
+            dividedParts[1] = text.Substring(index, word.Length);
+            dividedParts[2] = text.Substring(index + word.Length);
+
+            return dividedParts;
+        }
+
+        private string ReplaceLineBreaks(string input)
+        {
+            input = input.Replace("<br>", "\n");
+            input = System.Text.RegularExpressions.Regex.Replace(input, "<.{0,20}>", "");
+
+            /*input = input.Replace("<td>", "");
+            input = input.Replace("<tr>", "");
+            input = input.Replace("<b>", "");
+            input = input.Replace("<i>", "");
+            input = input.Replace("<font size=\"-1\">", "");
+            input = input.Replace("<font>", "");
+            input = input.Replace("<a>", "");
+            input = input.Replace("<p>", "");
+            input = input.Replace("</td>", "");
+            input = input.Replace("</tr>", "");
+            input = input.Replace("</b>", "");
+            input = input.Replace("</i>", "");
+            input = input.Replace("</font size=\"-1\">", "");
+            input = input.Replace("</font>", "");
+            input = input.Replace("</a>", "");
+            input = input.Replace("</p>", "");
+            input = input.Replace("<table>", "");
+            input = input.Replace("</table>", "");
+            input = input.Replace("<sup>", "");
+            input = input.Replace("</sup>", "");
+            input = input.Replace("<TD>", "");
+            input = input.Replace("</TD>", "");
+            input = input.Replace("<TR>", "");
+            input = input.Replace("</TR>", "");
+            input = input.Replace("<FONT size=\"-1\">", "");
+            input = input.Replace("</FONT>", "");
+            input = input.Replace("<tr valign=\"top\">", "");
+            input = input.Replace("<TR valign=\"top\">", "");*/
+
+            return input;
+        }
+
+        private ArrayList FindWordIndexesInText(string text, string word)
+        {
+            ArrayList foundWords = new ArrayList();
+            int lastIndex = 0;
+            wordsCount = 0;
+
+            while (lastIndex != -1)
+            {
+
+                lastIndex = text.IndexOf(word, lastIndex);
+
+                if (lastIndex != -1)
+                {
+                    foundWords.Add(lastIndex);
+                    lastIndex += 1;
+                    wordsCount++;
+                }
+                Dispatcher.Invoke(() => UpdateResultText("Znalezionych słów: " + wordsCount.ToString()));
+            }
+            return foundWords;
+        }
+
+        private ArrayList FindBindNumberStartEndIndexes(string text, ArrayList wordIndexes)
+        {
+            ArrayList bindNumberStartEndIndexes = new ArrayList();
+
+            for (int i = 0; i < wordIndexes.Count; i++)
+            {
+                int foundWordIndex = (int)wordIndexes[i];
+                int[] tmp = {   text.LastIndexOf("<b>Nummer:</b>", foundWordIndex),
+                                text.LastIndexOf("<FONT", foundWordIndex),
+                                text.LastIndexOf("Brevtekst", foundWordIndex),
+                                text.IndexOf("</p>", foundWordIndex) };
+                bindNumberStartEndIndexes.Add(tmp);
+            }
+
+            return bindNumberStartEndIndexes;
+        }
+
+        private List<int[]> RemoveDuplicates(ArrayList list)
+        {
+            HashSet<string> set = new HashSet<string>();
+            List<int[]> uniqueList = new List<int[]>();
+
+            foreach (int[] array in list)
+            {
+                // Convert the array to a string representation
+                string arrayStr = "[" + string.Join(",", array) + "]";
+
+                // Check if the set does not contain the string representation
+                if (!set.Contains(arrayStr))
+                {
+                    set.Add(arrayStr); // Add it to the set to mark it as seen
+                    uniqueList.Add(array); // Add the original array to the unique list
+                }
+            }
+
+            return uniqueList;
+        }
+
+        private void CreateTabsFromText(string text, List<int[]> bindNumberStartEndIndexes)
+        {
+            int numberIndex, bindIndex, textStartIndex, textEndIndex;
+            string numberValue, bindValue, textValue;
+
+            for (int i = 0; i < bindNumberStartEndIndexes.Count; i++)
+            {
+                numberIndex = bindNumberStartEndIndexes[i][0] + 14;
+                bindIndex = bindNumberStartEndIndexes[i][1] + 16;
+                textStartIndex = bindNumberStartEndIndexes[i][2];
+                textEndIndex = bindNumberStartEndIndexes[i][3];
+
+                numberValue = text.Substring(numberIndex, 7);
+                bindValue = text.Substring(bindIndex, 4);
+                textValue = text.Substring(textStartIndex, textEndIndex - textStartIndex);
+
+                numberValue = System.Text.RegularExpressions.Regex.Replace(numberValue, "[^0-9]", "");
+
+                textValue = ReplaceLineBreaks(textValue);
+                string[] dividedText = DivideText(textValue, word);
+
+                Dispatcher.Invoke(() => AddNewEntry("Bind: " + bindValue, "Number: " + numberValue, dividedText));
+            }
+        }
+
+        public void SearchForWord(string word)
+        {
+            ArrayList foundWords = new ArrayList();
+            ArrayList bindNumberStartEndIndexes = new ArrayList();
+
+            string path = "Texts/DIPLOMATARIUM.html"; 
+            string textContent = File.ReadAllText(path);
+
+            foundWords = FindWordIndexesInText(textContent, word);
+            Dispatcher.Invoke(() => UpdateResultText("Zapisywanie słów"));
+
+            bindNumberStartEndIndexes = FindBindNumberStartEndIndexes(textContent, foundWords);
+            Dispatcher.Invoke(() => UpdateResultText("Generowanie etykiet"));
+
+            List<int[]> filteredBindNumberStartEndIndexes = RemoveDuplicates(bindNumberStartEndIndexes);
+            CreateTabsFromText(textContent, filteredBindNumberStartEndIndexes);
+            Dispatcher.Invoke(() => UpdateResultText("Znalezione teksty: " + wordsCount.ToString()));
+
+            isSearching = false;
+        }
+    }
+}
